78a15e560e6aaa0defbdf67def6865e5f6901e22
Vanix <71411025+Vanix-k3rnel@users.noreply.github.com>
2021-06-14 10:34:26 +0200

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
 
+namespace ip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Pinger";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+        }
+
+          public static string ip()
+          {
+            WebClient kek = new WebClient();
+            string ip kek.DownloadString("https://api.ipify.org/?format=text");
+           return ip;
+          }
+
+    }
+}

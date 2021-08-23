28335ba54273590b07a3ba0e6631682f2fde00cc
Vanix <71411025+Vanix-k3rnel@users.noreply.github.com>
2021-08-23 22:06:59 +0200

    Update program.cs
    Fixed bugg

--- program.cs
+++ program.cs
@@ -11,28 +11,16 @@ namespace ip
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.Title = "Pinger";
-            // Console color
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            // going to the string that will get the ip. and write the ip to console
-            Console.WriteLine(ip);
-            Console.Readline();
-            
-            
-            
-            
-            
-            
+            Console.WriteLine(ip());
+            Console.ReadLine();
         }
-
-          public static string ip()
-          {
+        public static string ip()
+        {
             WebClient kek = new WebClient();
             string ip = kek.DownloadString("https://api.ipify.org/?format=text");
             return ip;
-          }
-
+        }
     }
 }
